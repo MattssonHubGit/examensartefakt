d11b20e05084acd76340d0f060c39bb04f9fbaaf
David Nilsson <ddavidov61@hotmail.com>
2019-02-18 17:03:58 +0100

    Player movement, enemy movement

---
+++ Project/DnDiablo/Assets/SimpleMoveTowardsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleMoveTowardsPlayer : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+    NavMeshObstacle obstacle;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+        obstacle = GetComponent<NavMeshObstacle>();
+        agent.Warp(transform.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        agent.destination = target.transform.position;
+
+
+    }
+}
