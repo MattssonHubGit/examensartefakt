18a0efa1bbfb8d359e4a51c8435b0f7e51391e30
MattssonHubGit <d16erima@student.his.se>
2019-02-21 11:00:22 +0100

    continued work on skills

--- Project/DnDiablo/Assets/Scripts/Player.cs
+++ Project/DnDiablo/Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : Entity {
 
     public List<Skill> mySkills = new List<Skill>();
 
+    private KeyCode[] skillKeys = new KeyCode[8] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
+
     private void Update()
     {
         SkillCooldownManager();
 
-        SkillController();
+        SkillInputController();
 
         if (Input.GetKeyDown(KeyCode.Keypad8))
         {
@@ -31,15 +33,65 @@ public class Player : Entity {
         }
     }
 
-    private void SkillController()
+    private void SkillInputController()
     {
-        /*for (int i = 0; i < mySkills.Count; i++)
+        for (int i = 0; i < mySkills.Count; i++)
         {
-            if (Input.GetButtonDown(i.ToString()))
+            //Limit to skillKeys length
+            if (i > skillKeys.Length)
             {
-                Debug.Log(i);
+                Debug.LogError("Player::SkillInputController -- Player has more skills than they have buttons! Skipping this itteration");
+                continue;
             }
-        }*/
+
+            //Attempt to use the skill corresponding to the button pressed button
+            if (Input.GetKeyDown(skillKeys[i]))
+            {
+                UseSkill(mySkills[i]);
+            }
+        }
+    }
+
+    protected void UseSkill(Skill skill)
+    {
+        if (skill.AttemptCast(this))
+        {
+            Debug.Log("Casting skill: " + skill.name);
+            skill.Action(GetPositionFromMouse(), this);
+        }
+        else
+        {
+            Debug.Log("SkillToExpensive: " + skill.name);
+        }
+    }
+
+    private Vector3 GetPositionFromMouse()
+    {
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+        // Generate a ray from the cursor position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Determine the point where the cursor ray intersects the plane.
+        // This will be the point that the object must look towards to be looking at the mouse.
+        // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+        //   then find the point along that ray that meets that distance.  This will be the point
+        //   to look at.
+        float hitdist = 0.0f;
+
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            // Get the point along the ray that hits the calculated distance.
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            return targetPoint;
+        }
+        else
+        {
+            Debug.LogError("Player::GetPositionFromMouse -- Raycast missed plane, returning Vector3.zero");
+            return Vector3.zero;
+        }
     }
 
     protected override void OnDeath()
