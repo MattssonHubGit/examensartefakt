f599e923029c4be02260a34a65aeb40d9cb5de4b
MattssonHubGit <erik.mattsson@live.se>
2019-02-22 14:46:19 +0100

    basic fireball movement

---
+++ Project/DnDiablo/Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [Header("Base Projectile")]
+    [SerializeField] public float speed;
+    [SerializeField] public Vector3 direction = new Vector3();
+
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
--- Project/DnDiablo/Assets/Scripts/Skills/Fireball.cs
+++ Project/DnDiablo/Assets/Scripts/Skills/Fireball.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class Fireball : Skill {
 
+    [Header("Skill Specific")]
+    [SerializeField] private GameObject spellObectPrefab;
+    [SerializeField] private float moveSpeed;
+
+
     public override void Action(Vector3 targetPos, Entity caster)
     {
-        Debug.Log("Fireball::Action -- Skill logic not implemented");
+        //Debug.Log("Fireball::Action -- Skill logic not implemented");
+
+        GameObject _objBall = Instantiate(spellObectPrefab, caster.transform.position, Quaternion.identity);
+        Projectile _projectile = _objBall.GetComponent<Projectile>();
+
+        Vector3 _dir = targetPos - caster.transform.position;
+        _dir.Normalize();
+        _projectile.direction = _dir;
+        _projectile.speed = moveSpeed;
     }
 }
