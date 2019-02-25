3404a895595b61f862e8d46b58e7561234dd4436
MattssonHubGit <d16erima@student.his.se>
2019-02-25 12:20:37 +0100

    started working on stat upgrading

---
+++ Project/DnDiablo/Assets/Scripts/UI/StatsUpgrader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsUpgrader : MonoBehaviour {
+
+    [Header("Stat Display Areas")]
+    [SerializeField] private Text healthTxt;
+    [SerializeField] private Text powerTxt;
+    [SerializeField] private Text resourceTxt;
+    [SerializeField] private Text cooldownTxt;
+    [SerializeField] private Text speedTxt;
+
+    [Header("Other")]
+    [SerializeField] private GameObject statsUpgradeParent;
+    [SerializeField] private KeyCode toggleKey = KeyCode.C;
+
+    #region Singleton
+    public static StatsUpgrader Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            ToggleStatsWindow();
+        }
+
+    }
+
+    public void UpdateStatsDisplay()
+    {
+        healthTxt.text = Player.Instance.myStats.healthBase.ToString();
+        powerTxt.text = Player.Instance.myStats.powerBase.ToString();
+        resourceTxt.text = Player.Instance.myStats.resourceBase.ToString();
+        cooldownTxt.text = Player.Instance.myStats.cooldownRedBase.ToString();
+        speedTxt.text = Player.Instance.myStats.moveSpeedBase.ToString();
+    }
+
+    public void ToggleStatsWindow()
+    {
+        UpdateStatsDisplay();
+        statsUpgradeParent.SetActive(!statsUpgradeParent.active);
+    }
+}
