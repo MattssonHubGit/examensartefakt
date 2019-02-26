d143102bda4c753bf06c5778d8d0a26efe65c8e3
MattssonHubGit <d16erima@student.his.se>
2019-02-26 14:14:57 +0100

    axe throwing

--- Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/AxeThrow.cs
+++ Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/AxeThrow.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
+//[CreateAssetMenu()]
 public class AxeThrow : Skill
 {
     [Header("Spell Specific")]
     [SerializeField] private GameObject axePrefab;
+    [Space]
     [SerializeField] private List<float> speedByLevel = new List<float>();
     [SerializeField] private List<bool> penetrateByLevel = new List<bool>();
+    [Space]
+    [SerializeField] private List<float> damageByLevel = new List<float>();
 
 
     public override void Action(Vector3 targetPos, Entity caster)
     {
-        Debug.Log("AxeThrow::Action() -- Skill logic not implemented yet.");
+        GameObject _objAxe = Instantiate(axePrefab, caster.transform.position, Quaternion.identity);
+        AxeThrowBehaviour _scrAxe = _objAxe.GetComponent<AxeThrowBehaviour>();
+
+
+        //Movement
+        Vector3 _dir = targetPos - caster.transform.position;
+        _dir.Normalize();
+        _scrAxe.direction = _dir;
+        _scrAxe.speed = speedByLevel[level];
+        _objAxe.transform.rotation = Quaternion.LookRotation(_dir);
+
+        //Stats
+        _scrAxe.damage = (damageByLevel[level] * caster.myStats.powerCurrent);
+
+        //Destroy object after duration is up
+        Destroy(_objAxe, duration[level]);
     }
 }
--- Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/AxeThrowBehaviour.cs
+++ Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/AxeThrowBehaviour.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public class AxeThrowBehaviour : MonoBehaviour {
 
+    [Header("Movement")]
     [HideInInspector] public float speed;
     [HideInInspector] public Vector3 direction;
     [HideInInspector] public bool penetrate = false;
+    [SerializeField] private float rotateSpeed;
+
+    [Header("Stats")]
+    [HideInInspector] public float damage;
+
+    [Header("Components")]
+    [SerializeField] private GameObject gfxParent;
+
+    private void Update()
+    {
+        Projectile.Movement(this.transform, direction, speed);
+        RotationController();
+    }
+
+    private void RotationController()
+    {
+        gfxParent.transform.RotateAround(gfxParent.transform.position, transform.right, rotateSpeed *Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Did I hit something damagable?
+        IDamageable _toDamage = other.gameObject.GetComponent<IDamageable>();
+        if (_toDamage != null)
+        {
+            _toDamage.TakeDamage(damage);
+            if (!penetrate) //Keep going if penetrating enemies
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else //Will not penetrate walls
+        {
+            Destroy(this.gameObject);
+        }
+        
+
+    }
 
 }
