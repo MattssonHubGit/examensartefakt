6a20fd311b74eeae9cfcc5d65d772ece2dc7a6bf
MattssonHubGit <d16erima@student.his.se>
2019-02-27 12:12:02 +0100

    skills

---
+++ Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/ArrowSpray.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ArrowSpray : Skill
+{
+
+    [Header("Spell Specific")]
+    [SerializeField] private GameObject arrowPrefab;
+    [Header("Movement")]
+    [SerializeField] private List<float> speedByLevel = new List<float>();
+    [SerializeField] [Range(3, 10)] private List<int> arrowsByLevel = new List<int>();
+    [SerializeField] private float fireIntervalls;
+    [SerializeField] private List<float> maxSpread = new List<float>();
+    [Space]
+    [SerializeField] private List<float> damageByLevel = new List<float>();
+    
+
+    public override void Action(Vector3 targetPos, Entity caster)
+    {
+        bool isEvenAmount = (arrowsByLevel[level] % 2 == 0); //Do we have a even amount of arrows?
+        float angleOffSet = (arrowsByLevel[level] / maxSpread[level]); //Angle for arrows
+        
+
+        for (int i = 0; i < arrowsByLevel[level]; i++)
+        {
+            bool currentIsEven = (i % 2 == 0);
+
+            //Calculate arrow angle offset based on amount of arrows, max spread, and current arrow created
+            if (isEvenAmount)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            GameObject _objArrow = Instantiate(arrowPrefab, caster.transform.position, Quaternion.identity);
+            ArrowSprayBehaviour _scrArrow = _objArrow.GetComponent<ArrowSprayBehaviour>();
+
+            //Movement
+            Vector3 _dir = targetPos - caster.transform.position;
+            _dir.Normalize();
+            _scrArrow.direction = _dir;
+            _scrArrow.speed = speedByLevel[level];
+            _objArrow.transform.rotation = Quaternion.LookRotation(_dir);
+
+            //Stats
+            _scrArrow.damage = (damageByLevel[level] * caster.myStats.powerCurrent);
+
+            //Destroy object after duration is up
+            Destroy(_objArrow, duration[level]);
+        }
+    }
+}
---
+++ Project/DnDiablo/Assets/Scripts/Skills/PlayerSkills/CripplingShotBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CripplingShotBehaviour : MonoBehaviour
+{
+
+    [Header("Movement")]
+    [HideInInspector] public float speed;
+    [HideInInspector] public Vector3 direction;
+    [SerializeField] private float rotateSpeed;
+
+    [Header("Stats")]
+    [HideInInspector] public float damage;
+    [HideInInspector] public bool slow;
+    [HideInInspector] public float slowPercentage;
+    [HideInInspector] public float slowDuration;
+
+
+    [Header("Components")]
+    [SerializeField] private SlowAura slowPrefab;
+    [SerializeField] private GameObject gfxParent;
+    
+
+    private void Update()
+    {
+        Projectile.Movement(this.transform, direction, speed);
+        RotationController();
+        
+    }
+
+    private void RotationController()
+    {
+        gfxParent.transform.RotateAround(gfxParent.transform.position, transform.forward, rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Did I hit something damagable?
+        IDamageable _toDamage = other.gameObject.GetComponent<IDamageable>();
+        if (_toDamage != null)
+        {
+            //Apply effects
+            _toDamage.TakeDamage(damage);
+
+            SlowAura _slow = Instantiate(slowPrefab);
+            _slow.Duration = slowDuration;
+            _slow.slowPercentage = slowPercentage;
+
+            Entity _target = other.GetComponent<Entity>();
+            if (_target != null)
+            {
+                _target.AddAura(_slow);
+            }
+
+        }
+
+
+        Destroy(this.gameObject);
+    }
+}
