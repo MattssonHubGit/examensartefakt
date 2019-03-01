c6f3e130a6f2480aedeed28b400bb73b7d74647c
MattssonHubGit <d16erima@student.his.se>
2019-03-01 11:35:00 +0100

    Level Manager and main menu WIP

--- Project/DnDiablo/Assets/Scripts/LevelManager.cs
+++ Project/DnDiablo/Assets/Scripts/LevelManager.cs
@@ -2,32 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
+    
 
-    //Class currently only used for debugging
+    [Header("Settings")]
+    private int currentLevel = 0;
+    private List<Scene> allLevelScenes = new List<Scene>();
 
+
+    [Header("Debug")]
     [SerializeField] private string sceneName = "";
-
     [SerializeField] private KeyCode key = KeyCode.Keypad7;
 
     public static LevelManager Instance;
 
-    private void Awake()
+    private void Start()
     {
+        #region Singleton
         //Singleton
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
+            
+
+            //Get all level scenes and put them in a list
+            for (int i = 0; i < SceneManager.sceneCountInBuildSettings - 1; i++)
+            {
+                Debug.Log(i);
+                if (SceneManager.GetSceneByBuildIndex(i).name.Contains("Level_"))
+                {
+                    allLevelScenes.Add(SceneManager.GetSceneByBuildIndex(i));
+                }
+            }
+
         }
         else
         {
             Destroy(this.gameObject);
         }
+        #endregion
     }
 
     void Update()
+    {
+        LoadDebugScene();
+    }
+
+    public void LoadNextLevel()
+    {
+        currentLevel++;
+        string _nextLevelName = "Level_" + currentLevel;
+
+        if (currentLevel > allLevelScenes.Count)
+        {
+            currentLevel = 0;
+            SceneManager.LoadScene("MainMenu");
+            return;
+        }
+
+        SceneManager.LoadScene(_nextLevelName);
+
+    }
+
+    private void LoadDebugScene()
     {
         if (Input.GetKeyDown(key))
         {
@@ -35,5 +75,10 @@ public class LevelManager : MonoBehaviour {
         }
     }
 
+    public void ExitGame()
+    {
+        Debug.Log("Application.Quit()");
+        Application.Quit();
+    }
 
 }
