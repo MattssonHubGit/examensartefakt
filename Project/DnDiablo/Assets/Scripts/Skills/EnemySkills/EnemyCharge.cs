0582436f56b66db742ee123d0d794c8fd6cbb486
David Nilsson <ddavidov61@hotmail.com>
2019-03-11 12:22:15 +0100

    Added skills to enemies again

---
+++ Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyCharge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharge : Skill {
+
+    [SerializeField] private float chargeRange;
+    [SerializeField] private float damage;
+    [SerializeField] private EnemyChargeAura chargeAura;
+
+
+    public override void Action(Vector3 targetPos, Entity caster)
+    {
+
+    }
+
+    
+}
---
+++ Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyChargeAura.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChargeAura : Aura {
+
+    public Entity caster;
+    public NavMeshAgent agent;
+    public Vector3 endPos;
+
+    public override void OnApply()
+    {
+        agent = caster.GetComponent<NavMeshAgent>();
+        agent.speed =
+    }
+
+    public override void OnExpire()
+    {
+
+    }
+
+    public override void OnTick()
+    {
+
+    }
+}
