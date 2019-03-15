7c57ecbe58aa42511ebd3154c7027427d58f6e74
David Nilsson <ddavidov61@hotmail.com>
2019-03-15 16:09:53 +0100

    Healer and particles for teleport

--- Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyEscapeTeleport.cs
+++ Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyEscapeTeleport.cs
@@ -8,16 +8,21 @@ public class EnemyEscapeTeleport : Skill {
 
     [Header("Skill specifics")]
     [SerializeField] private float teleportDistance;
+    [SerializeField] private GameObject teleportEffects;
+    [SerializeField] private float effectsDuration;
 
     public override void Action(Vector3 targetPos, Entity caster)
     {
         Vector3 _dir = targetPos - caster.transform.position;
         _dir.Normalize();
+        
+        GameObject _effects = Instantiate(teleportEffects, caster.transform.position, Quaternion.identity);
 
         Vector3 _teleportPos = caster.transform.position - (_dir * teleportDistance);
 
         caster.gameObject.GetComponent<NavMeshAgent>().Warp(_teleportPos);
 
+        Destroy(_effects, effectsDuration);
     }
 
     
--- Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyHealAOE.cs
+++ Project/DnDiablo/Assets/Scripts/Skills/EnemySkills/EnemyHealAOE.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class EnemyHealAOE : Skill {
 
     [Header("Skill Specifics")]
-    [SerializeField] private float healAmount;
+    [SerializeField] private float healAmountPerSecond;
     [SerializeField] private GameObject healGO;
 
 
     public override void Action(Vector3 targetPos, Entity caster)
     {
-        GameObject _heal = Instantiate(healGO, caster.transform.position, Quaternion.identity);
+        GameObject _heal = Instantiate(healGO, caster.transform.position, Quaternion.identity, caster.transform);
         EnemyHealAOEBehaviour _EHB = _heal.GetComponent<EnemyHealAOEBehaviour>();
-        _EHB.healAmount = healAmount;
+        _EHB.healAmount = healAmountPerSecond;
         _EHB.duration = duration[0];
         _EHB.caster = caster;
         
